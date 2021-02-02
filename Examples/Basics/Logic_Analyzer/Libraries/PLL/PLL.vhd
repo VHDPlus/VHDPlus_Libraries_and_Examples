2c30dac8bf5bc0b88f07c26d798427ce689f6426
Leon Beier <lbeier@vhdplus.com>
2021-02-02 13:08:22 +0100

    Update Logic Analyzer

--- Examples/Basics/Logic_Analyzer/Libraries/PLL/PLL.vhd
+++ Examples/Basics/Logic_Analyzer/Libraries/PLL/PLL.vhd
@@ -135,7 +135,7 @@ BEGIN
 		bandwidth_type => "AUTO",
 		clk0_divide_by => 3,
 		clk0_duty_cycle => 50,
-		clk0_multiply_by => 25,
+		clk0_multiply_by => 50,
 		clk0_phase_shift => "0",
 		compensate_clock => "CLK0",
 		inclk0_input_frequency => 83333,
@@ -217,7 +217,7 @@ END SYN;
 -- Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "Any"
 -- Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
 -- Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
--- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "100.000000"
+-- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "200.000000"
 -- Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 -- Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 -- Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -242,7 +242,7 @@ END SYN;
 -- Retrieval info: PRIVATE: MIRROR_CLK0 STRING "0"
 -- Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
 -- Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
--- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "100.00000000"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "200.00000000"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
@@ -287,7 +287,7 @@ END SYN;
 -- Retrieval info: CONSTANT: BANDWIDTH_TYPE STRING "AUTO"
 -- Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "3"
 -- Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
--- Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "25"
+-- Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "50"
 -- Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "0"
 -- Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 -- Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "83333"
@@ -347,7 +347,7 @@ END SYN;
 -- Retrieval info: GEN_FILE: TYPE_NORMAL PLL.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL PLL.ppf TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL PLL.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL PLL.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL PLL.cmp FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL PLL.bsf FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL PLL_inst.vhd FALSE
 -- Retrieval info: LIB_FILE: altera_mf
