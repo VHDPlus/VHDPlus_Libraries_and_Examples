d7f962adda46382497adfc6d4d74eae868c0875e
Hendrik Mennen <hmennen@vhdplus.com>
2021-03-10 12:25:25 +0100

    publishscript

---
+++ PublishLibraries.bat
@@ -0,0 +1,14 @@
+SET version=1.2
+Set OUTPUT=publish
+
+::Clear output
+MKDIR %OUTPUT%
+del /f /s /q %OUTPUT% 1>nul
+
+powershell Compress-Archive -DestinationPath %OUTPUT%\Libraries_%version%.zip -Path Libraries\* -Force
+echo %version% https://cdn.vhdplus.com/vhdpluslibraries/Libraries_%version%.zip > %OUTPUT%\VersionInfoWin64.txt
+echo %version% https://cdn.vhdplus.com/vhdpluslibraries/Libraries_%version%.zip > %OUTPUT%\VersionInfoLinux64.txt
+
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /ini=nul /script=UploadLibraries.txt
+
+PAUSE
