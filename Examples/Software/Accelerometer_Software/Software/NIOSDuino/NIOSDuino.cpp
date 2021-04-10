3d444f49cda038f7d93af77986bc1f8ce40b4ea3
Leon Beier <lbeier@vhdplus.com>
2021-04-10 15:42:43 +0200

    output X, Y and Z

--- Examples/Software/Accelerometer_Software/Software/NIOSDuino/NIOSDuino.cpp
+++ Examples/Software/Accelerometer_Software/Software/NIOSDuino/NIOSDuino.cpp
@@ -5,13 +5,18 @@
 LIS3DH accel( SPI_MODE, 15 );
 
 void setup() {
-    Serial.begin(9600); //Set Baudrate with "New Processor"
+    Serial0.begin(9600); //Set Baudrate with "New Processor"
     
     accel.begin();
 }
 
+char message[32];
 void loop() {
-    Serial.println("Hello World");
-    Serial.println(accel.readFloatAccelX(), 4);
-    delay(1000);
+    sprintf(message, "X: %.5f", accel.readFloatAccelX());
+    Serial0.println(message);
+    sprintf(message, "Y: %.5f", accel.readFloatAccelY());
+    Serial0.println(message);
+    sprintf(message, "Z: %.5f", accel.readFloatAccelZ());
+    Serial0.println(message);
+    delay(100);
 }
